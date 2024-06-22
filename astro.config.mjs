dfb5f12c60b33b3a11cb3d5c8fcd295c95842b5d
Timon Vonk <timonv@gmail.com>
2024-06-22 14:00:30 +0200

    feat: basics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,33 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Swiftide",
+      editLink: {
+        baseUrl: "https://github.com/bosun-ai/swiftide-website/edit/master",
+      },
+      // site: "https://swiftide.bosun.ai",
+      // integrations: [starlight({ title: "Swiftide" })],
+      logo: { src: "./src/assets/logo.png" },
+      social: {
+        github: "https://github.com/bosun-ai/swiftide",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
