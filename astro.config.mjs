0327a6943883db4181cca69a4936ac3fc93ba1f2
Timon Vonk <timonv@gmail.com>
2024-06-22 14:15:29 +0200

    feat: fonts and transparent logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://swiftide.bosun.ai",
   integrations: [
     starlight({
-      title: "Swiftide",
+      title: "swiftide",
       editLink: {
         baseUrl: "https://github.com/bosun-ai/swiftide-website/edit/master",
       },
-      // site: "https://swiftide.bosun.ai",
-      // integrations: [starlight({ title: "Swiftide" })],
+      customCss: [
+        // Fontsource files for to regular and semi-bold font weights.
+        "@fontsource/fira-code/400.css",
+        "@fontsource/fira-code/600.css",
+        "./src/styles/custom.css",
+      ],
       logo: { src: "./src/assets/logo.png" },
       social: {
         github: "https://github.com/bosun-ai/swiftide",
