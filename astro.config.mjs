51c4b4e23a684d83ded20c9a3a2e72cd3130aaf4
Timon Vonk <timonv@gmail.com>
2024-08-13 14:29:46 +0200

    Add plausible.io with partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-
 import tailwind from "@astrojs/tailwind";
 import starlightLinksValidatorPlugin from "starlight-links-validator";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,86 +12,82 @@ export default defineConfig({
     service: {
       entrypoint: "astro/assets/services/sharp",
       config: {
-        limitInputPixels: false,
-      },
-    },
+        limitInputPixels: false
+      }
+    }
   },
   prefetch: {
-    prefetchAll: true,
+    prefetchAll: true
   },
-  integrations: [
-    starlight({
-      title: "swiftide",
-      description:
-        "Blazing fast data pipelines for Retrieval Augmented Generation written in Rust",
-      editLink: {
-        baseUrl: "https://github.com/bosun-ai/swiftide-website/edit/master",
+  integrations: [starlight({
+    title: "swiftide",
+    // <script defer data-domain="swiftide.rs" src="https://plausible.io/js/script.js"></script>
+    head: [{
+      tag: "script",
+      attrs: {
+        defer: true,
+        type: "text/partytown",
+        "data-domain": "swiftide.rs",
+        src: "https://plausible.io/js/script.js"
+      }
+    }],
+    description: "Blazing fast data pipelines for Retrieval Augmented Generation written in Rust",
+    editLink: {
+      baseUrl: "https://github.com/bosun-ai/swiftide-website/edit/master"
+    },
+    tableOfContents: {
+      minHeadingLevel: 2
+    },
+    customCss: [
+    // Fontsource files for to regular and semi-bold font weights.
+    "@fontsource/fira-code/400.css", "@fontsource/fira-code/600.css", "./src/styles/custom.css", "./src/tailwind.css"],
+    favicon: "favicon32.png",
+    plugins: [starlightLinksValidatorPlugin()],
+    logo: {
+      src: "./src/assets/logo.png"
+    },
+    social: {
+      github: "https://github.com/bosun-ai/swiftide",
+      linkedin: "https://www.linkedin.com/company/bosun-ai/"
+    },
+    sidebar: [{
+      label: "What is swiftide?",
+      link: "/what-is-swiftide/"
+    }, {
+      label: "Getting Started",
+      autogenerate: {
+        directory: "getting-started"
+      }
+    }, {
+      label: "Concepts",
+      autogenerate: {
+        directory: "concepts"
+      }
+    }, {
+      label: "In depth",
+      autogenerate: {
+        directory: "in-depth"
+      }
+    }, {
+      label: "Tutorials",
+      autogenerate: {
+        directory: "tutorials"
+      }
+    }, {
+      label: "Reference",
+      link: "https://docs.rs/swiftide/latest/swiftide/",
+      badge: {
+        variant: "note",
+        text: "docs.rs"
       },
-      tableOfContents: {
-        minHeadingLevel: 2,
-      },
-      customCss: [
-        // Fontsource files for to regular and semi-bold font weights.
-        "@fontsource/fira-code/400.css",
-        "@fontsource/fira-code/600.css",
-        "./src/styles/custom.css",
-        "./src/tailwind.css",
-      ],
-      favicon: "favicon32.png",
-      plugins: [starlightLinksValidatorPlugin()],
-      logo: {
-        src: "./src/assets/logo.png",
-      },
-      social: {
-        github: "https://github.com/bosun-ai/swiftide",
-        linkedin: "https://www.linkedin.com/company/bosun-ai/",
-      },
-      sidebar: [
-        {
-          label: "What is swiftide?",
-          link: "/what-is-swiftide/",
-        },
-        {
-          label: "Getting Started",
-          autogenerate: {
-            directory: "getting-started",
-          },
-        },
-        {
-          label: "Concepts",
-          autogenerate: {
-            directory: "concepts",
-          },
-        },
-        {
-          label: "In depth",
-          autogenerate: {
-            directory: "in-depth",
-          },
-        },
-        {
-          label: "Tutorials",
-          autogenerate: {
-            directory: "tutorials",
-          },
-        },
-
-        {
-          label: "Reference",
-          link: "https://docs.rs/swiftide/latest/swiftide/",
-          badge: {
-            variant: "note",
-            text: "docs.rs",
-          },
-
-          attrs: { target: "_blank" },
-        },
-        {
-          label: "Troubleshooting",
-          link: "/troubleshooting/",
-        },
-      ],
-    }),
-    tailwind({ applyBaseStyles: false }),
-  ],
+      attrs: {
+        target: "_blank"
+      }
+    }, {
+      label: "Troubleshooting",
+      link: "/troubleshooting/"
+    }]
+  }), tailwind({
+    applyBaseStyles: false
+  }), partytown()]
 });
